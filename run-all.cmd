effaab8078ecdc82ff192f0a1cfb4f7fec6ba131
Do Minh Hieu <hieudm@ichiba.vn>
2022-02-28 15:21:37 +0700

    UP

---
+++ run-all.cmd
@@ -0,0 +1,2 @@
+start  dotnet watch run --project "/src/OMS/OMS.csproj" 
+start  dotnet watch run --project  "/src/OMS/GatewayServices/GatewayServices.csproj"  
---
+++ src/run-all.cmd
@@ -0,0 +1,2 @@
+start  dotnet run --project "OMS\OMS.csproj"   
+start  dotnet run --project GatewayServices\GatewayServices.csproj"   
